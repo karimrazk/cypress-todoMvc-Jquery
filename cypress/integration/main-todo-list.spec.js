13dc29f6173a69ffc8ab01bb5d84e6ac8ab1a773
MehdiZahwa <zahouani.mehdi06@gmail.com>
2020-04-06 23:42:16 +0000

    feat: add remove all items -- chore: refactoring

--- cypress/integration/main-todo-list.spec.js
+++ cypress/integration/main-todo-list.spec.js
@@ -1,43 +1,31 @@
 const myItemOne="MyItem 1";
 const myItemTwo="MyItem 2";
 const myItemThree="MyItem 3";
+const removeOne="DelItem 1";
+const removeTwo="DelItem 2";
+const updateText="First Item";
 
 describe("Items list tests", () => {
 
-  before(   () => {
+  beforeEach(   () => {
     cy.visit("/");
-    cy.get(".new-todo").type(myItemOne + "{enter}");
-    cy.get(".new-todo").type(myItemTwo + "{enter}");    
-    cy.get(".new-todo").type(myItemThree + "{enter}");
+
   });
 
-  context("Adding items itest", () => {
+  context("Adding items test", () => {
 
     it("Check if 3 items are added !", () => {
+      cy.get(".new-todo").type(myItemOne + "{enter}");
+      cy.get(".new-todo").type(myItemTwo + "{enter}");    
+      cy.get(".new-todo").type(myItemThree + "{enter}");
+
       cy.get('.todo-list li')
           .should('have.length', 3)
-          .and('contain', 'MyItem')
+          .and('contain', myItemOne)
+          .and('contain', myItemTwo)
+          .and('contain', myItemThree)
           .find('.toggle')
           .should('not.be.checked')
-    });
-
-  });
-
-  context("Completed/Active items tests", () => {
-
-    it("Should assigne " + myItemOne + " to Completed category", () => {
-    cy.contains(".todo-list li", myItemOne).find(".toggle").check();
-    cy.get('.todo-list li')
-        .filter('.completed')
-        .should('have.length', 1)
-    cy.contains(".todo-list li", myItemOne).should("have.class", "completed");
-    });
- 
-
-    it("Should assigne " + myItemOne + " to Active category", () => {
-      cy.contains(".todo-list li", myItemOne).find(".toggle").uncheck();
-      cy.contains(".todo-list li", myItemOne).find(".toggle").should('not.be.checked');
-      cy.contains(".todo-list li", myItemOne).should("not.have.class", "completed");
 
     });
 
@@ -46,49 +34,88 @@ describe("Items list tests", () => {
 
   context("Remove items tests", () => {
 
-    it("Should remove " + myItemTwo, () => {
-      cy.contains(".todo-list li", myItemTwo)
+    it("Should remove DelItem 1", () => {
+      cy.get(".new-todo").type(removeOne + "{enter}");
+      cy.get(".new-todo").type(removeTwo + "{enter}");
+
+      cy.contains(".todo-list li", removeOne)
           .find(".destroy")
           .invoke("show")
           .click();
       
       cy.get('.todo-list li')
-          .should('have.length', 2)
-          .and('not.contain', myItemTwo)
+          .should('have.length', 1)
+          .and('not.contain', removeOne)
 
     });
 
 
+
+
     it("Should remove item when it's an empty text ", () => {
-      cy.contains(".todo-list li", myItemThree)
+      cy.get(".new-todo").type(removeTwo + "{enter}");
+
+      cy.contains(".todo-list li", removeTwo)
           .find(".edit")
           .invoke("show")
           .type("{selectall}  {del} {enter}");
   
-      cy.contains(".todo-list li", myItemThree)
+      cy.contains(".todo-list li", removeTwo)
           .should('not.exist');   
     });
+
+
+
+    it("Should remove all items !", () => {
+
+      cy.get(".new-todo").type(removeOne + "{enter}");
+      cy.get(".new-todo").type(removeTwo + "{enter}");
+
+     
+      cy.get(".todo-list li").each(function () {
+        cy.get(".destroy")
+          .invoke("show")
+          .click({ force: true, multiple: true });
+      });
+      cy.get('.todo-list li')
+          .should('have.length', 0)
+    });
+
 
   });
 
 
   context("Update item test", () => {
 
-    it("Should update " + myItemOne + " to First Item ", () => {
+    it("Should update MyItem 1 to First Item ", () => {
+      cy.get(".new-todo").type(myItemOne + "{enter}");
       cy.contains(".todo-list li", myItemOne)
           .find(".edit")
           .invoke("show")
-          .type("{selectall} First Item {enter}");
+          .type("{selectall}"+ updateText +"{enter}");
 
       cy.contains(".todo-list li", myItemOne)
           .should('not.exist');   
-      cy.contains(".todo-list li", "First Item")
+      cy.contains(".todo-list li", updateText)
           .should('exist');
     });
     
   });
 
 
+  context("Completed items test", () => {
 
+
+    it("Should assigne MyItem 1 to Completed category", () => {
+      cy.get(".new-todo").type(myItemOne + "{enter}");
+
+      cy.contains(".todo-list li", myItemOne).find(".toggle").check();
+      cy.get('.todo-list li')
+          .filter('.completed')
+          .should('have.length', 1)
+      cy.contains(".todo-list li", myItemOne).should("have.class", "completed");
+    });
+
+  });
 
 });
