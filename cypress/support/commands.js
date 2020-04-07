68668d0c85cec2f7f1507c083664d8c45dc31c46
karimrazok <50910945+karimrazk@users.noreply.github.com>
2020-04-07 11:32:52 +0100

    chore : refactoring footer spec

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,10 @@
-Cypress.Commands.add("addListItems", () => {
-  cy.get(".new-todo").type("item 1  {enter}").should("have.value", "");
-  cy.get(".new-todo").type("item 2  {enter}").should("have.value", "");
-  cy.get(".new-todo").type("item 3  {enter}").should("have.value", "");
-  cy.get(".new-todo").type("item 4  {enter}").should("have.value", "");
-  cy.contains(".todo-list li", "item 2").find(".toggle").check();
-  cy.contains(".todo-list li", "item 4").find(".toggle").check();
-});
-
-
-Cypress.Commands.add('fill', (...label) => {
-  label.forEach(element => {
-      cy.get(".new-todo").type(element + "{enter}");
+Cypress.Commands.add("fill", (...label) => {
+  label.forEach((element) => {
+    cy.get(".new-todo").type(element + "{enter}");
   });
- })
+});
+Cypress.Commands.add("completedItems", (...label) => {
+  label.forEach((element) => {
+    cy.contains(".todo-list li", element).find(".toggle").check();
+  });
+});
