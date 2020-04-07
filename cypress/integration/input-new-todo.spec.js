469c9ef27e3b51b46649aa68d89f99c1ebf378af
Raji Ayoub <ayoubossraji@gmail.com>
2020-04-07 12:02:06 +0100

    chore:refactor add new todo functionalité

--- cypress/integration/input-new-todo.spec.js
+++ cypress/integration/input-new-todo.spec.js
@@ -1,17 +1,19 @@
 describe("add new todo test", () => {
-  const textToType = "learn cypress";
+    const textToType = "learn cypress";
 
-  beforeEach(() => {
-    cy.visit("/");
-  });
+    beforeEach(() => {
+        cy.visit("/");
+    });
 
-  it("should add new todo to the list", () => {
-    cy.get("input[class=new-todo]")
-      .type(textToType + "{enter}")
-      .should("have.value", "");
+    it("should add new todo to the list", () => {
+        cy.get("input[class=new-todo]")
+            .type(textToType + "{enter}")
+            .should("have.value", "");
 
-    cy.get("ul[class=todo-list] > li")
-      .should("have.length", 1)
-      .and("contain", textToType);
-  });
+        cy.get("ul[class=todo-list] > li")
+            .should("have.length", 1)
+            .and("contain", textToType)
+            .find(".toggle")
+            .should("not.be.checked");
+    });
 });
