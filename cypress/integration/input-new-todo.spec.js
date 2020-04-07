950e6253f4c435fb8d2df6adcea37f4effc59ca2
Raji Ayoub <ayoubossraji@gmail.com>
2020-04-07 13:07:14 +0100

    feat:toggle all items test

--- cypress/integration/input-new-todo.spec.js
+++ cypress/integration/input-new-todo.spec.js
@@ -1,5 +1,4 @@
 describe("add new todo test", () => {
-    const textToType = "learn cypress";
 
     beforeEach(() => {
         cy.visit("/");
@@ -7,13 +6,26 @@ describe("add new todo test", () => {
 
     it("should add new todo to the list", () => {
         cy.get("input[class=new-todo]")
-            .type(textToType + "{enter}")
+            .type("learn cypress" + "{enter}")
             .should("have.value", "");
 
         cy.get("ul[class=todo-list] > li")
             .should("have.length", 1)
-            .and("contain", textToType)
+            .and("contain", "learn cypress")
             .find(".toggle")
             .should("not.be.checked");
+    });
+
+    it("Should toggle all items ", () => {
+
+        cy.fill("learn js ", "learn ecma");
+        cy.get("label").contains("Mark all as complete").click();
+
+        cy.get("ul[class=todo-list] > li").should(($list) => {
+            expect($list).to.have.length(2);
+            expect($list.eq(0)).to.contain("learn js").to.have.class("completed");
+            expect($list.eq(1)).to.contain("learn ecma").to.have.class("completed");
+        });
+
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,10 @@
 Cypress.Commands.add("fill", (...label) => {
-  label.forEach((element) => {
-    cy.get(".new-todo").type(element + "{enter}");
-  });
+    label.forEach((element) => {
+        cy.get(".new-todo").type(element + "{enter}");
+    });
 });
 Cypress.Commands.add("completedItems", (...label) => {
-  label.forEach((element) => {
-    cy.contains(".todo-list li", element).find(".toggle").check();
-  });
+    label.forEach((element) => {
+        cy.contains(".todo-list li", element).find(".toggle").check();
+    });
 });
